d8dc4571257fe75abc50bbf4bdbc535a8934a107
Brayden <braydenk03@gmail.com>
2023-04-09 20:33:37 -0700

    Update DownloadVideoIds.js

--- TikTok/DownloadVideoIds.js
+++ TikTok/DownloadVideoIds.js
@@ -1,9 +1,84 @@
 // ==UserScript==
-// @name         Edge's TikTok VideoID Grabber (Obfuscated)
+// @name         Edge's TikTok VideoID Grabber
 // @version      1.0
 // @description  VideoID Downloader for Edge's TikTok Downloader
 // @author       Edge
 // @match        https://www.tiktok.com/*
 // @grant        none
 // ==/UserScript==
-function _0x3bfd(_0x4fcec4,_0x34cbac){const _0x17e833=_0x17e8();return _0x3bfd=function(_0x3bfdda,_0x5ace2e){_0x3bfdda=_0x3bfdda-0x143;let _0x4917f9=_0x17e833[_0x3bfdda];return _0x4917f9;},_0x3bfd(_0x4fcec4,_0x34cbac);}(function(_0x1f3f81,_0x580c45){const _0x227d28=_0x3bfd,_0x5bc060=_0x1f3f81();while(!![]){try{const _0x48d467=-parseInt(_0x227d28(0x15c))/0x1+-parseInt(_0x227d28(0x159))/0x2+parseInt(_0x227d28(0x154))/0x3*(parseInt(_0x227d28(0x145))/0x4)+-parseInt(_0x227d28(0x149))/0x5*(-parseInt(_0x227d28(0x144))/0x6)+-parseInt(_0x227d28(0x156))/0x7*(-parseInt(_0x227d28(0x160))/0x8)+-parseInt(_0x227d28(0x162))/0x9+-parseInt(_0x227d28(0x148))/0xa;if(_0x48d467===_0x580c45)break;else _0x5bc060['push'](_0x5bc060['shift']());}catch(_0x3a2005){_0x5bc060['push'](_0x5bc060['shift']());}}}(_0x17e8,0x313a8),(function(){'use strict';const _0x1994d1=_0x3bfd;window[_0x1994d1(0x168)](_0x1994d1(0x14a),()=>{const _0x31a461=_0x1994d1,_0x231109=document[_0x31a461(0x166)](_0x31a461(0x153));_0x231109[_0x31a461(0x151)]='\x0a\x20\x20\x20\x20\x20\x20\x20\x20button.download-tiktok-videos\x20{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20background:\x20linear-gradient(#231E3C,\x20#231E3C)\x20padding-box,\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x09linear-gradient(to\x20right,\x20#08E2D8,\x20#3D18BB)\x20border-box\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20border:\x201px\x20solid\x20transparent\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20color:\x20#fff\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20padding:\x2010px\x2020px\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20font-size:\x2016px\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20font-weight:\x20600\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20border-radius:\x2030px\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20margin:\x2010px\x200\x20!important;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20cursor:\x20pointer;\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20transition:\x20all\x200.3s\x20ease;\x0a\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x0a\x20\x20\x20\x20\x20\x20\x20\x20button.download-tiktok-videos:hover\x20{\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20opacity:\x200.8;\x0a\x20\x20\x20\x20\x20\x20\x20\x20}\x0a\x20\x20\x20\x20',document['head']['appendChild'](_0x231109);const _0xbb0eeb=document[_0x31a461(0x146)](_0x31a461(0x14c)),_0x4f1417=document[_0x31a461(0x166)](_0x31a461(0x157));_0x4f1417[_0x31a461(0x163)]=_0x31a461(0x165),_0x4f1417[_0x31a461(0x155)]['add'](_0x31a461(0x15f)),_0xbb0eeb[_0x31a461(0x167)]['insertBefore'](_0x4f1417,_0xbb0eeb[_0x31a461(0x161)]),_0x4f1417[_0x31a461(0x168)](_0x31a461(0x14f),async function(){const _0x39c278=_0x31a461;let _0x2c004d=0x0,_0x559957=document[_0x39c278(0x14e)][_0x39c278(0x150)];while(_0x2c004d!==_0x559957){window[_0x39c278(0x15a)](0x0,_0x559957),await new Promise(_0x4f3790=>setTimeout(_0x4f3790,0x3e8)),_0x2c004d=_0x559957,_0x559957=document[_0x39c278(0x14e)]['scrollHeight'];}const _0x4c30c2=document[_0x39c278(0x164)]('a'),_0x142362=[];for(const _0x324b5d of _0x4c30c2){const _0x45c8ab=/^https?:\/\/www\.tiktok\.com\/@[^/]+\/video\/(\d+)/i,_0x3e3147=_0x324b5d[_0x39c278(0x147)][_0x39c278(0x14d)](_0x45c8ab);_0x3e3147&&_0x142362[_0x39c278(0x158)](_0x3e3147[0x1]);}const _0x20e061=new Blob([_0x142362[_0x39c278(0x152)]('\x0a')],{'type':_0x39c278(0x143)}),_0x50970d=URL[_0x39c278(0x15e)](_0x20e061),_0x363767=document[_0x39c278(0x166)]('a');_0x363767[_0x39c278(0x147)]=_0x50970d,_0x363767['download']=_0x39c278(0x14b),_0x363767['textContent']=_0x39c278(0x15b),document['body'][_0x39c278(0x15d)](_0x363767),_0x363767[_0x39c278(0x14f)](),_0x363767['remove']();});});}()));function _0x17e8(){const _0x54e060=['innerHTML','join','style','6IxoIrt','classList','14qbsUrW','button','push','24922QWQTcd','scrollTo','Download\x20TikTok\x20Video\x20IDs','67488doPjUr','appendChild','createObjectURL','download-tiktok-videos','774064kZJLzX','nextSibling','98226VeWcdH','textContent','querySelectorAll','Download\x20All\x20Video\x20IDs','createElement','parentNode','addEventListener','text/plain','6fSiHwq','615056UUUtob','querySelector','href','3529960GUvTcB','722275aVqhtV','load','tiktok_video_ids.txt','div[data-e2e=\x22user-more\x22]','match','body','click','scrollHeight'];_0x17e8=function(){return _0x54e060;};return _0x17e8();}
+
+(function() {
+    'use strict';
+
+    // Wait until the page is fully loaded before running
+    window.addEventListener('load', () => {
+        const styleBlock = document.createElement('style');
+        styleBlock.innerHTML = `
+        button.download-tiktok-videos {
+            background: linear-gradient(#231E3C, #231E3C) padding-box,
+            	linear-gradient(to right, #08E2D8, #3D18BB) border-box !important;
+            border: 1px solid transparent !important;
+            color: #fff !important;
+            padding: 10px 20px !important;
+            font-size: 16px !important;
+            font-weight: 600 !important;
+            border-radius: 30px !important;
+            margin: 10px 0 !important;
+            cursor: pointer;
+            transition: all 0.3s ease;
+        }
+
+        button.download-tiktok-videos:hover {
+            opacity: 0.8;
+        }
+    `;
+    document.head.appendChild(styleBlock);
+        // Add a button after the <div data-e2e="user-more"> element
+        const userMoreDiv = document.querySelector('div[data-e2e="user-more"]');
+        const button = document.createElement('button');
+        button.textContent = 'Download All Video IDs';
+        button.classList.add('download-tiktok-videos');
+        userMoreDiv.parentNode.insertBefore(button, userMoreDiv.nextSibling);
+
+        // Add an event listener to the button to trigger the video download process
+        button.addEventListener('click', async function() {
+            // Scroll to the bottom of the page until it can't scroll anymore
+            let previousScrollHeight = 0;
+            let currentScrollHeight = document.body.scrollHeight;
+
+            while (previousScrollHeight !== currentScrollHeight) {
+                window.scrollTo(0, currentScrollHeight);
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                previousScrollHeight = currentScrollHeight;
+                currentScrollHeight = document.body.scrollHeight;
+            }
+
+            // Get all <a> tags on the page
+            const links = document.querySelectorAll("a");
+
+            // Loop through each <a> tag
+            const videoIds = [];
+            for (const link of links) {
+                // Check if the link matches the TikTok video URL pattern
+                const regex = /^https?:\/\/www\.tiktok\.com\/@[^/]+\/video\/(\d+)/i;
+                const match = link.href.match(regex);
+
+                // If the link matches, add the video ID to the list
+                if (match) {
+                    videoIds.push(match[1]);
+                }
+            }
+
+            // Create a blob from the video IDs and save it to a text file
+            const blob = new Blob([videoIds.join('\n')], { type: 'text/plain' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'tiktok_video_ids.txt';
+            a.textContent = 'Download TikTok Video IDs';
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+        });
+    });
+})();
